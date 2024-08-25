1327eec2f4cc417209a83b363077e83794129aa1
mahbub maruf <mdmahbubalom178@gmail.com>
2024-08-26 00:06:57 +0600

    update db

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,10 +1,23 @@
-import type { NextRequest } from 'next/server'
-import { getRequestContext } from '@cloudflare/next-on-pages'
-
-export const runtime = 'edge'
+import type { NextRequest } from "next/server";
+import { getRequestContext } from "@cloudflare/next-on-pages";
+import { Client } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { sql } from "drizzle-orm";
+export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
-  let responseText = 'Hello World'
+  let responseText = "Hello World";
+  const client = new Client({
+    connectionString:
+      "postgresql://sllj1f:xau_D1iXaZbeN4WYHR57Xx56ow1cGjRdTxwy@us-east-1.sql.xata.sh/premiumzone:main?sslmode=require",
+  });
+  await client.connect();
+  const db = drizzle(client);
+  // response all table name
+  // Execute the SQL query to get all table names from the 'public' schema
+  const tables = await db.execute(
+    sql`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`
+  );
 
   // In the edge runtime you can use Bindings that are available in your application
   // (for more details see:
@@ -18,5 +31,9 @@ export async function GET(request: NextRequest) {
   // const suffix = await myKv.get('suffix')
   // responseText += suffix
 
-  return new Response(responseText)
+  return new Response(JSON.stringify({ tables }), {
+    headers: {
+      "content-type": "application/json",
+    },
+  });
 }
