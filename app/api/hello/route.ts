2091341aa5aa9e285eba598016b202fc43b2ee45
mahbub maruf <mdmahbubalom178@gmail.com>
2024-08-26 00:45:31 +0600

    error handle

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -6,37 +6,45 @@ import { sql } from "drizzle-orm";
 export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
-  let responseText = "Hello World";
-  // const client = new Client({
-  //   connectionString:
-  //     "postgresql://sllj1f:xau_D1iXaZbeN4WYHR57Xx56ow1cGjRdTxwy@us-east-1.sql.xata.sh/premiumzone:main?sslmode=require",
-  // });
-  // await client.connect();
+  try {
+    let responseText = "Hello World";
+    // const client = new Client({
+    //   connectionString:
+    //     "postgresql://sllj1f:xau_D1iXaZbeN4WYHR57Xx56ow1cGjRdTxwy@us-east-1.sql.xata.sh/premiumzone:main?sslmode=require",
+    // });
+    // await client.connect();
 
-  const client = getRequestContext().env.XATA_CLIENT;
+    const client = getRequestContext().env.XATA_CLIENT;
 
-  const db = drizzle(client);
-  // response all table name
-  // Execute the SQL query to get all table names from the 'public' schema
-  const tables = await db.execute(
-    sql`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`
-  );
+    const db = drizzle(client);
+    // response all table name
+    // Execute the SQL query to get all table names from the 'public' schema
+    const tables = await db.execute(
+      sql`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`
+    );
 
-  // In the edge runtime you can use Bindings that are available in your application
-  // (for more details see:
-  //    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
-  //    - https://developers.cloudflare.com/pages/functions/bindings/
-  // )
-  //
-  // KV Example:
-  // const myKv = getRequestContext().env.MY_KV_NAMESPACE
-  // await myKv.put('suffix', ' from a KV store!')
-  // const suffix = await myKv.get('suffix')
-  // responseText += suffix
+    // In the edge runtime you can use Bindings that are available in your application
+    // (for more details see:
+    //    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
+    //    - https://developers.cloudflare.com/pages/functions/bindings/
+    // )
+    //
+    // KV Example:
+    // const myKv = getRequestContext().env.MY_KV_NAMESPACE
+    // await myKv.put('suffix', ' from a KV store!')
+    // const suffix = await myKv.get('suffix')
+    // responseText += suffix
 
-  return new Response(JSON.stringify({ tables }), {
-    headers: {
-      "content-type": "application/json",
-    },
-  });
+    return new Response(JSON.stringify({ tables }), {
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error }), {
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  }
 }
